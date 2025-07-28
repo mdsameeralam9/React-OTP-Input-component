bbfd279c8c2dce3c7089f3a6f99cbdb926599964
mdsameeralam9 <mdsameer.alam9@gmail.com>
2025-07-28 21:03:10 +0530

    add typescript to resend Timer OTP componenet

--- src/components/ReactOTP/Resend.tsx
+++ src/components/ReactOTP/Resend.tsx
@@ -3,21 +3,33 @@ import './style.css';
 import useResendOTP from './hooks/useResendOTP';
 import { getMinutesAndSeconds, formattedTime } from './util';
 
-interface ResendProps {
+interface CommonStyleProps {
+  renderTimeClass?: string;
+  renderTimeStyle?: React.CSSProperties;
+  resendButtonClass?: string;
+  resendButtonStyle?: React.CSSProperties;
+  resendDisplayLabelStyle?: React.CSSProperties;
+  resendButtonLabelStyle?: React.CSSProperties;
+  resendButtonLabelClass?: string;
+  resendDisplayLabelClass?: string;
+  timerLabelStyle?: React.CSSProperties;
+  displayRemainingTimeStyle?: React.CSSProperties;
+  resendTimerClass?: string;
+  resendTextClass?: string;
+}
+
+interface ResendProps extends CommonStyleProps {
   resendButtonLabel?: string;
   resendDisplayLabel?: string;
   maxTime?: number;
   onResendClick: () => void;
   className?: string;
-  renderTime?: ({ }) => React.ReactNode;
-  renderResendButton?: (handleResendClick: any) => React.ReactNode;
-  timerLabel?: string,
-
-  // handle style 
-  renderTimeClass?: string
-  renderTimeStyle?: React.CSSProperties;
-  resendButtonClass?: string;
-  resendButtonStyle?: React.CSSProperties;
+  renderTime?: (args: {
+    remainingTime: number;
+    displayRemainingTime: string;
+  }) => React.ReactNode;
+  renderResendButton?: (handleResendClick: () => void) => React.ReactNode;
+  timerLabel?: string;
 }
 
 export const ResendOTP: React.FC<ResendProps> = ({
@@ -28,79 +40,85 @@ export const ResendOTP: React.FC<ResendProps> = ({
 }) => {
   const { remainingTime, isTimerActive, handleResendClick } = useResendOTP(props);
   const { minutes, seconds } = getMinutesAndSeconds(remainingTime);
-
   const displayRemainingTime = `${formattedTime(minutes)}:${formattedTime(seconds)}`;
 
   return (
     <div className={`resendComponent ${className}`} role="timer" aria-live="polite">
       {isTimerActive ? (
         <Fragment>
-          {renderTime ? renderTime({ remainingTime, displayRemainingTime }) :
+          {renderTime ? (
+            renderTime({ remainingTime, displayRemainingTime })
+          ) : (
             <RenderTimeDefault
               displayRemainingTime={displayRemainingTime}
               {...props}
             />
-          }
+          )}
         </Fragment>
-
       ) : (
         <Fragment>
-          {renderResendButton ? renderResendButton(handleResendClick) :
+          {renderResendButton ? (
+            renderResendButton(handleResendClick)
+          ) : (
             <ResendButtonDefault
               handleResendClick={handleResendClick}
               {...props}
             />
-          }
+          )}
         </Fragment>
       )}
     </div>
   );
-}
+};
 
-const RenderTimeDefault = ({
-  timerLabel = "Resend OTP in",
-  displayRemainingTime = "",
-  renderTimeClass = "",
+const RenderTimeDefault: React.FC<
+  {
+    timerLabel?: string;
+    displayRemainingTime?: string;
+  } & CommonStyleProps
+> = ({
+  timerLabel = 'Resend OTP in',
+  displayRemainingTime = '',
+  renderTimeClass = '',
   renderTimeStyle = {},
   timerLabelStyle = {},
   displayRemainingTimeStyle = {},
   resendTimerClass = '',
-  resendTextClass = ""
-}) => {
-  return (
-    <div className={`renderTimeDefaultClass ${renderTimeClass && renderTimeClass}`}
-      style={renderTimeStyle}
-    >
-      <span className={`resend-text ${resendTextClass && resendTextClass}`} style={timerLabelStyle}>{timerLabel}</span>
-      <span className={`resend-timer ${resendTimerClass && resendTimerClass}`} style={displayRemainingTimeStyle}>{displayRemainingTime}</span>
+  resendTextClass = '',
+}) => (
+    <div className={`renderTimeDefaultClass ${renderTimeClass}`} style={renderTimeStyle}>
+      <span className={`resend-text ${resendTextClass}`} style={timerLabelStyle}>
+        {timerLabel}
+      </span>
+      <span className={`resend-timer ${resendTimerClass}`} style={displayRemainingTimeStyle}>
+        {displayRemainingTime}
+      </span>
     </div>
-  )
-}
+  );
 
-
-const ResendButtonDefault = ({
-  resendButtonLabel = "Resend OTP",
+const ResendButtonDefault: React.FC<
+  {
+    resendButtonLabel?: string;
+    resendDisplayLabel?: string;
+    handleResendClick?: () => void;
+  } & CommonStyleProps
+> = ({
+  resendButtonLabel = 'Resend OTP',
   resendDisplayLabel = "Didn't receive the otp?",
   handleResendClick = () => { },
-  resendButtonClass = "",
+  resendButtonClass = '',
   resendButtonStyle = {},
   resendDisplayLabelStyle = {},
   resendButtonLabelStyle = {},
-  resendButtonLabelClass = "",
-  resendDisplayLabelClass = ""
-}) => {
-  return (
-    <div className={`renderTimeDefaultClass ${resendButtonClass && resendButtonClass}`}
-      style={resendButtonStyle}
-    >
-      <span
-        className={`resend-text ${resendDisplayLabelClass && resendDisplayLabelClass}`}
-        style={resendDisplayLabelStyle}
-      >
+  resendButtonLabelClass = '',
+  resendDisplayLabelClass = '',
+}) => (
+    <div className={`renderTimeDefaultClass ${resendButtonClass}`} style={resendButtonStyle}>
+      <span className={`resend-text ${resendDisplayLabelClass}`} style={resendDisplayLabelStyle}>
         {resendDisplayLabel}
       </span>
       <button
-        className={`resend-button ${resendButtonLabelClass && resendButtonLabelClass}`}
+        className={`resend-button ${resendButtonLabelClass}`}
         onClick={handleResendClick}
         aria-label={resendButtonLabel}
         type="button"
@@ -109,8 +127,4 @@ const ResendButtonDefault = ({
         {resendButtonLabel}
       </button>
     </div>
-  )
-}
-
-
-
+  );
